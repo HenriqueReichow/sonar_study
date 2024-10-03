8534c9758e6480b08855deddb74616687e6b08a7
HenriqueReichow <reichowhenrique@gmail.com>
2024-10-03 15:24:52 -0300

    roll fix

--- sonar_data_code.py
+++ sonar_data_code.py
@@ -1,26 +1,27 @@
 #import holoocean
 import numpy as np
 import json
-#import matplotlib.pyplot as plt
 import open3d as o3d
-#import cupy as np
 
-def traducao(raio,azimuth,elev):
-    x = raio * np.cos(azimuth) * np.sin(np.radians(elev))
-    y = raio * np.sin(azimuth) * np.sin(np.radians(elev))
-    z = raio * np.cos(np.radians(elev))
-    return x,y,z
+def traducao(raio,azimuth,elev,roll): 
+    x = (raio * np.cos(azimuth) * np.sin(elev))
+    y = (raio * np.sin(azimuth) * np.sin(elev))
+    z = (raio * np.cos(elev))
+
+    y_roll = y * np.cos(roll) - z * np.sin(roll)
+    z_roll = y * np.sin(roll) + z * np.cos(roll)
+    return x,y_roll,z_roll
 
 for n in range(40):
     x_coord,y_coord,z_coord = [],[],[]
     for k in range(1,200):
             try:
-                dados = np.load(f"/home/lh/Desktop/mission-1/auv-{n}-data/{n}-raw-sonar-data-{k}.npy")
+                dados = np.load(f"/home/lh/Documents/mission-1/auv-{n}-data/{n}-raw-sonar-data-{k}.npy")
 
             except FileNotFoundError as error:
                   break
             else:
-                with open(f'/home/lh/Desktop/mission-1/auv-{n}-data/{n}-sonar_meta_data-{k}.json','r') as arquivo:
+                with open(f'/home/lh/Documents/mission-1/auv-{n}-data/{n}-sonar_meta_data-{k}.json','r') as arquivo:
                         dados_json = json.load(arquivo)
 
                 azi = dados_json["sonar_azimuth"]
@@ -39,9 +40,9 @@ for n in range(40):
                 distances = np.linspace(minR, maxR, binsR)
                 angulos = np.radians(np.linspace(-azi/2 + yaw, azi/2 + yaw, binsA))
                 dist_vals,angul_vals = np.meshgrid(distances,angulos,indexing="ij")
-                x,y,z = traducao(dist_vals, angul_vals, 90 - pitch)
+                x,y,z = traducao(dist_vals, angul_vals, np.radians(90 - pitch), np.radians(roll))
 
-                mask_points = dados > np.max(dados) * 0.8
+                mask_points = dados > np.max(dados) * 0.8 
 
                 x_coord.append((x[mask_points] + sonar_pos_x).flatten())
                 y_coord.append((y[mask_points] + sonar_pos_y).flatten())
@@ -55,6 +56,6 @@ for n in range(40):
 
     pcd = o3d.geometry.PointCloud()
     pcd.points = o3d.utility.Vector3dVector(cloud)
-    o3d.io.write_point_cloud(f"nuvem_sonar_mission-1_{n}.xyz", pcd)
-    pcd_load = o3d.io.read_point_cloud(f"nuvem_sonar_mission-1_{n}.xyz")
+    o3d.io.write_point_cloud(f"/home/lh/Documents/clouds/mission-1/nuvem_sonar_mission-1_{n}.xyz", pcd)
+    pcd_load = o3d.io.read_point_cloud(f"/home/lh/Documents/clouds/mission-1/nuvem_sonar_mission-1_{n}.xyz")
     o3d.visualization.draw_geometries([pcd_load])
