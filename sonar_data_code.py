4eb7dd2ddf26e714f2f4b5c5e57add659794579a
HenriqueReichow <reichowhenrique@gmail.com>
2024-10-07 17:31:50 -0300

    bug fix and two roll options

--- sonar_data_code.py
+++ sonar_data_code.py
@@ -4,24 +4,24 @@ import json
 import open3d as o3d
 
 def traducao(raio,azimuth,elev,roll): 
-    x = (raio * np.cos(azimuth) * np.sin(elev))
-    y = (raio * np.sin(azimuth) * np.sin(elev))
-    z = (raio * np.cos(elev))
+    x = raio * np.cos(azimuth) * np.sin(elev)
+    y = raio * np.sin(azimuth) * np.sin(elev)
+    z = raio * np.cos(elev)
 
-    y_roll = y * np.cos(roll) - z * np.sin(roll)
-    z_roll = y * np.sin(roll) + z * np.cos(roll)
-    return x,y_roll,z_roll
+    #y_roll = y * np.cos(roll) - z * np.sin(roll)
+    #z_roll = y * np.sin(roll) + z * np.cos(roll)
+    return x,y,z
 
 for n in range(40):
     x_coord,y_coord,z_coord = [],[],[]
     for k in range(1,200):
             try:
-                dados = np.load(f"/home/lh/Documents/mission-1/auv-{n}-data/{n}-raw-sonar-data-{k}.npy")
+                dados = np.load(f"/home/lh/Documents/mission-3/auv-{n}-data/{n}-raw-sonar-data-{k}.npy")
 
             except FileNotFoundError as error:
                   break
             else:
-                with open(f'/home/lh/Documents/mission-1/auv-{n}-data/{n}-sonar_meta_data-{k}.json','r') as arquivo:
+                with open(f'/home/lh/Documents/mission-3/auv-{n}-data/{n}-sonar_meta_data-{k}.json','r') as arquivo:
                         dados_json = json.load(arquivo)
 
                 azi = dados_json["sonar_azimuth"]
@@ -37,12 +37,15 @@ for n in range(40):
                 roll = dados_json["roll"]
 
                 #tentando transformar em pointcloud
-                distances = np.linspace(minR, maxR, binsR)
-                angulos = np.radians(np.linspace(-azi/2 + yaw, azi/2 + yaw, binsA))
+                distances = np.linspace(minR, maxR, len(dados))
+                angulos = np.radians(np.linspace(-azi/2 + yaw, azi/2 + yaw, len(dados[:][0])))
                 dist_vals,angul_vals = np.meshgrid(distances,angulos,indexing="ij")
                 x,y,z = traducao(dist_vals, angul_vals, np.radians(90 - pitch), np.radians(roll))
-
                 mask_points = dados > np.max(dados) * 0.8 
+                print("nro linhas ",len(dados[:][0]))
+                print("nro col ",len(dados))
+                print(dados.shape)
+                
 
                 x_coord.append((x[mask_points] + sonar_pos_x).flatten())
                 y_coord.append((y[mask_points] + sonar_pos_y).flatten())
@@ -53,6 +56,9 @@ for n in range(40):
     z_coord = np.concatenate(z_coord)   
 
     cloud = np.column_stack((x_coord,y_coord,z_coord)) 
+    cloud = np.asarray(cloud)
+    cloud = cloud@(np.array([[ 1, 0, 0],[ 0, np.cos(roll), -np.sin(roll)], [0, np.sin(roll), np.cos(roll)]])).T
+    
 
     pcd = o3d.geometry.PointCloud()
     pcd.points = o3d.utility.Vector3dVector(cloud)
